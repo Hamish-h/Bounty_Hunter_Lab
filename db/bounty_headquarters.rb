91349fa5ff1c6236a010d6dcfa766d2e2b089f1f
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-24 15:46:37 +0100

    file structure created

---
+++ db/bounty_headquarters.rb
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS bounty;
+
+CREATE TABLE bountys(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  bounty_value INT2,
+  last_known_location VARCHAR(255),
+  favourite_weapon VARCHAR(255)
+  );
