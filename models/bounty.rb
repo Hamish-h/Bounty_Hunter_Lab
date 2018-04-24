1f5234fed12de3fc5594e71109ead2c7ba3354c9
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-24 15:25:32 +0100

    initial files created and class set

---
+++ models/bounty.rb
@@ -0,0 +1,11 @@
+class Bounty
+
+attr_accessor :name, :bounty_value, :last_known_location, :favourite_weapon
+
+def initialize(options)
+  @id = options["id"].to_i if options["id"]
+  @name = options["name"]
+  @bounty_value = options["bounty_value"].to_i
+  @last_known_location = options["last_known_location"]
+  @favourite_weapon = options["favourite_weapon"]
+end
